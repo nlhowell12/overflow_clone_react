6a34d190ca81d5329a732dc15e2c2f1dd53cf91f
Nicolas Howell <nlhowell@Nicolass-MBP.localdomain>
2018-12-20 11:42:47 -0500

    first commit, added material ui, react routing, added a theme (pulled from offerings, can change later, added .env changing the relative path directory to src/)'

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#0066ff',
+            main: '#3fb542',
+            contrastText: 'white',
+        },
+        secondary: {
+            main: '#2196f3'
+        }
+    }
+})
+
+ReactDOM.render(
+    <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+            <Switch>
+                <Route exact path='/' component={App}/>
+            </Switch>
+        </MuiThemeProvider>
+    </BrowserRouter>
+,    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
