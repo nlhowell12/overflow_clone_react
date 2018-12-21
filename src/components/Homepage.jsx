f99e9a381fe29984c50cac11c3f520d8f77c981e
Jake Berg <jacobvictorberg@gmail.com>
2018-12-21 11:57:30 -0500

    Added FilterButtons component, added fetch to filter buttons

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 import CommonContainer from 'components/CommonContainer'
+import FilterButtons from './FilterButtons';
 
 
 class Homepage extends Component {
@@ -16,24 +17,29 @@ class Homepage extends Component {
         const response = await fetch('http://localhost:8000/questions/')
         let questions = await response.json()
         console.log(questions)
-        this.setState({questions: questions.results})
+        this.setState({ questions: questions.results })
     }
 
     componentWillMount = () => {
         this.getQuestions()
     }
 
+    handleclick = async (query) => {
+        const response = await fetch('http://localhost:8000/questions/' + query)
+        let questions = await response.json()
+        this.setState({ questions: questions.results })
+    };
+
     render() {
         const { questions } = this.state;
         const { history } = this.props;
         return (
             <CommonContainer>
-                    <Paper>
-                    {questions.map(question => {
-                        return <Question key={question.body} question={question}/>
-                    })}    
-                    </Paper>
-                    <Button onClick={evt => history.push('/newQuestion')}>Ask a Question!</Button>
+                <Button onClick={evt => history.push('/newQuestion')}>Ask a Question!</Button>
+                <FilterButtons handleclick={this.handleclick} />
+                {questions.map(question => {
+                    return <Question key={question.body} question={question} />
+                })}
             </CommonContainer>
         )
     }
